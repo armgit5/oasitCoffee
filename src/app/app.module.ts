883f332472bc1804c51cd990b3505a58e72046aa
Arm Suwansiri <ysuwansiri@yahoo.com>
2016-09-28 10:47:21 +0700

    added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule} from "@angular/router";
+import {routeConfig,navigatableComponents} from "./app.routing";
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ...navigatableComponents
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routeConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import { CoffeeComponent } from "./coffee/coffee.component";
+
+export const routeConfig = [
+  { path: "", component: CoffeeComponent }
+];
+
+export const navigatableComponents = [
+  CoffeeComponent
+];
