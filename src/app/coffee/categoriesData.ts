ac8702d27b078549fa5f4467fa739da5a019d0cb
Arm Suwansiri <ysuwansiri@yahoo.com>
2016-09-30 13:45:37 +0700

    added a plut minus box

---
+++ src/app/coffee/categoriesData.ts
@@ -0,0 +1,27 @@
+
+
+export const categoriesData = {
+
+    categories: [
+        {
+            id:1,
+            name: "coffee"
+        },
+        {
+            id:2,
+            name: "menu"
+        },
+        {
+            id:3,
+            name: "special menu"
+        },
+        {
+            id:4,
+            name: "frappe"
+        },
+       
+    ]
+
+};
+
+
