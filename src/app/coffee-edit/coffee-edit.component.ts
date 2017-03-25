26f46be8b2d3db91ba7c5db29c8da3a62a94f196
Arm Suwansiri <ysuwansiri@yahoo.com>
2017-03-25 20:50:52 +0700

    finish creating basic coffee edit form

--- src/app/coffee-edit/coffee-edit.component.ts
+++ src/app/coffee-edit/coffee-edit.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs/Rx';
 import { Coffee } from '../coffees/coffee';
 import { AngularFire } from 'angularfire2';
 import { CoffeeService } from '../coffees/coffee.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-coffee-edit',
@@ -12,12 +13,14 @@ import { CoffeeService } from '../coffees/coffee.service';
 })
 export class CoffeeEditComponent implements OnInit {
 
+  coffeeForm: FormGroup;
   private subscription: Subscription;
   private coffee: Coffee;
   constructor(private router: Router,
               private route: ActivatedRoute,
               private af: AngularFire,
-              private coffeeService: CoffeeService) { }
+              private coffeeService: CoffeeService,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     // Load coffee from firebase
@@ -27,11 +30,23 @@ export class CoffeeEditComponent implements OnInit {
           this.coffeeService.loadCoffee(params['id']).subscribe(
             coffee => {
               this.coffee = coffee;
+              this.initForm();
             }
           );
         }
     });
-    
+  }
+
+  private initForm() {
+    this.coffeeForm = this.formBuilder.group({
+      name: [this.coffee.name, Validators.required],
+      imagePath: [this.coffee.url, Validators.required],
+      price: [this.coffee.price, Validators.required]
+    });
+  }
+
+  onSubmit() {
+    console.log('submit');
   }
 
 }
