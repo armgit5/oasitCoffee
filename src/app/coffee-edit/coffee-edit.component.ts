a49af1cb50949fb7128c05c27d7c4c13583d3b84
Arm Suwansiri <ysuwansiri@yahoo.com>
2017-03-26 20:41:58 +0700

    update img src in real time

--- src/app/coffee-edit/coffee-edit.component.ts
+++ src/app/coffee-edit/coffee-edit.component.ts
@@ -55,7 +55,7 @@ export class CoffeeEditComponent implements OnInit {
       this.firebaseApp = firebaseApp;
 
       this.imageUrl = "https://firebasestorage.googleapis.com/v0/b/oasit-b6bc8.appspot.com/o/images%2F-KfgNGPXpr5QuABGbS8y?alt=media&token=19f88df4-a805-4c05-932a-7ab7bdd4b56d";
-     
+      // console.log("old image url" + this.imageUrl);
   } 
 
   ngOnInit() {
@@ -95,7 +95,7 @@ export class CoffeeEditComponent implements OnInit {
     this.croppedHeight =bounds.bottom-bounds.top;
     this.croppedWidth = bounds.right-bounds.left;
     // console.log("cropped " + this.data1.image);
-    
+    // this.imageUrl = "https://firebasestorage.googleapis.com/v0/b/oasit-b6bc8.appspot.com/o/images%2F-KfgNGPXpr5QuABGbS8y1?alt=media&token=48c168ec-25c7-4eb7-bc09-f5a96a78185d";
   }
 
   fileChangeListener($event) {
@@ -110,22 +110,18 @@ export class CoffeeEditComponent implements OnInit {
     };
 
     myReader.readAsDataURL(file);
-    }
+  }
+  
 
   test() {
     console.log('submit');
     let test = this.data1.image.split("base64,");
-    console.log(test[1]);
-    var uploadTask = this.storageRef.putString(test[1], 'base64');
-
-    // Register three observers:
-    // 1. 'state_changed' observer, called any time the state changes
-    // 2. Error observer, called on failure
-    // 3. Completion observer, called on successful completion
-    uploadTask.on('state_changed', function(snapshot){
+    // console.log(test[1]);
+    this.storageRef = this.firebaseApp.storage().ref().child('images/' + this.coffee.$key + 1);
+    let uploadTask = this.storageRef.putString(test[1], 'base64').then(snapshot => {
       // Observe state change events such as progress, pause, and resume
       // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
       console.log('Upload is ' + progress + '% done');
       switch (snapshot.state) {
         case firebase.storage.TaskState.PAUSED: // or 'paused'
@@ -135,13 +131,11 @@ export class CoffeeEditComponent implements OnInit {
           console.log('Upload is running');
           break;
       }
-    }, function(error) {
-      // Handle unsuccessful uploads
-    }, function() {
-      // Handle successful uploads on complete
-      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-      var downloadURL = uploadTask.snapshot.downloadURL;
+      let downloadURL = snapshot.downloadURL;
+      this.imageUrl = downloadURL;
       console.log(downloadURL);
+    }).catch(error => {
+      // Handle unsuccessful uploads
     });
   }
 
