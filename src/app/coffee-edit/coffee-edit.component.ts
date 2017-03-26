8959ef0adc3cf72b025eaa772f57e9d7b289e799
Arm Suwansiri <ysuwansiri@yahoo.com>
2017-03-26 17:49:29 +0700

    can upload image to firebase

--- src/app/coffee-edit/coffee-edit.component.ts
+++ src/app/coffee-edit/coffee-edit.component.ts
@@ -22,10 +22,13 @@ export class CoffeeEditComponent implements OnInit {
   croppedHeight:number;
   sdkDb: any;
   image: string;
+  storageRef: any;
+  firebaseApp: any;
 
   @ViewChild('cropper', undefined) cropper:ImageCropperComponent;
 
   coffeeForm: FormGroup;
+  private coffeeId: string = "";
   private subscription: Subscription;
   private coffee: Coffee;
   constructor(private router: Router,
@@ -49,12 +52,11 @@ export class CoffeeEditComponent implements OnInit {
 
       this.data1 = {};
 
-      const storageRef = firebaseApp.storage().ref().child('image/6186.jpg');
-      storageRef.getDownloadURL().then(url => {
-        this.image = url;
-        console.log(url);
-      });
-  }
+      this.firebaseApp = firebaseApp;
+
+    
+     
+  } 
 
   ngOnInit() {
     // Load coffee from firebase
@@ -65,6 +67,15 @@ export class CoffeeEditComponent implements OnInit {
             coffee => {
               this.coffee = coffee;
               this.initForm();
+
+              // test downloading image from firebase
+              console.log(this.coffee.$key);
+              this.coffeeId = this.coffee.$key;
+
+              this.storageRef = this.firebaseApp.storage().ref().child('images/' + this.coffee.$key);
+              this.storageRef.getDownloadURL().then(function(url) {
+                console.log(url);
+              });
             }
           );
         }
@@ -83,7 +94,8 @@ export class CoffeeEditComponent implements OnInit {
   cropped(bounds:Bounds) {
     this.croppedHeight =bounds.bottom-bounds.top;
     this.croppedWidth = bounds.right-bounds.left;
-    console.log("cropped " + this.data1.image);
+    // console.log("cropped " + this.data1.image);
+    
   }
 
   fileChangeListener($event) {
@@ -100,8 +112,37 @@ export class CoffeeEditComponent implements OnInit {
     myReader.readAsDataURL(file);
     }
 
-  onSubmit() {
+  test() {
     console.log('submit');
+    let test = this.data1.image.split("base64,");
+    console.log(test[1]);
+    var uploadTask = this.storageRef.putString(test[1], 'base64');
+
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on('state_changed', function(snapshot){
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, function(error) {
+      // Handle unsuccessful uploads
+    }, function() {
+      // Handle successful uploads on complete
+      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      var downloadURL = uploadTask.snapshot.downloadURL;
+      console.log(downloadURL);
+    });
   }
 
 
