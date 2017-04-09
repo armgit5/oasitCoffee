22d76a0d6090dc32cd708472718509db0d8ad4d3
Arm Suwansiri <ysuwansiri@yahoo.com>
2017-04-09 21:40:27 +0700

    test ngx-bootstrap modal 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,26 +11,7 @@ import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'a
                 <my-header [count]="cartItems.length"></my-header>
                 <router-outlet></router-outlet>
               </div>
-
-              <button type="button" class="btn btn-primary" (click)="staticModal.show()">Static modal</button>
- 
-              <div class="modal fade" bsModal #staticModal="bs-modal" [config]="{backdrop: 'static'}"
-                  tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-                <div class="modal-dialog"> 
-                  <div class="modal-content" style="width:500px;height:250px;margin:0px;margin-left:5%">
-                    <div class="modal-header">
-                      <h4 class="modal-title pull-left">Static modal</h4>
-                      <button type="button" class="close pull-right" aria-label="Close" (click)="staticModal.hide()">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-                    <div class="modal-body">
-                      This is static modal, backdrop click will not close it.
-                      Click <b>&times;</b> to close modal.
-                    </div>
-                  </div>
-                </div>
-              </div>
+             
             `,
   styleUrls: ['./app.component.css']
 })
