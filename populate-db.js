ab1d2e3fbf4fcc2ae8b41a4d0ddc7aba86b98036
Arm Suwansiri <ysuwansiri@yahoo.com>
2017-04-20 23:51:53 +0700

    save in github

--- populate-db.js
+++ populate-db.js
@@ -1,11 +1,7 @@
 var firebase = require('firebase');
 var dbData = require("./db-data");
 var app = firebase.initializeApp({
-    apiKey: "AIzaSyC2-Bt2eP64CxHbztz6YLomHHz0PkvzYnw",
-    authDomain: "oasit-b6bc8.firebaseapp.com",
-    databaseURL: "https://oasit-b6bc8.firebaseio.com",
-    storageBucket: "oasit-b6bc8.appspot.com",
-    messagingSenderId: "334270939180"
+   
 });
 
 var coffeeRef = firebase.database().ref('coffees');
