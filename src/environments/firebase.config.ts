c6411cddcd99312765239b299a054b70f03b5596
Arm Suwansiri <ysuwansiri@yahoo.com>
2017-04-22 21:04:51 +0700

    save to github

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,11 +1,7 @@
 import {AuthMethods, AuthProviders} from "angularfire2/index";
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyC2-Bt2eP64CxHbztz6YLomHHz0PkvzYnw",
-    authDomain: "oasit-b6bc8.firebaseapp.com",
-    databaseURL: "https://oasit-b6bc8.firebaseio.com",
-    storageBucket: "oasit-b6bc8.appspot.com",
-    messagingSenderId: "334270939180"
+   
 };
 
 export const authConfig = {
