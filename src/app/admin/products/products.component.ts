c60220996e7de6374bb464f44bb9c999d940183f
Arm Suwansiri <ysuwansiri@yahoo.com>
2017-09-02 19:02:59 +0700

    for testing admin page

--- src/app/admin/products/products.component.ts
+++ src/app/admin/products/products.component.ts
@@ -13,7 +13,37 @@ declare var $: any;
 export class ProductsComponent implements OnInit {
 
 
-  testData = ["Categories", "Attributes"];
+  testData = [
+  {
+    text: "Categories",
+    children: [{
+      text: "Coffee",
+      children: [
+        "Hot",
+        "Cold",
+        "Shake"
+      ]
+    },
+      "Foods",
+      "Drinks",
+      "Cakes"
+    ],
+    state: {
+      selected: true
+    }
+  },
+  {
+    text: "Attributes",
+    children: [
+      {
+        text: "SM",
+        children: []
+      },
+      "MD",
+      "LG"
+    ],
+    max_children: 1
+  }];
 
   constructor() { }
 
@@ -21,17 +51,27 @@ export class ProductsComponent implements OnInit {
     $('#tree').jstree({
       "core" : {
           "animation" : 0,
-          "check_callback" : true,
+          "check_callback" : (operation, node, node_parent, node_position) => {
+
+            console.log(operation, node, node_parent, node_position);
+            if (operation == "create_node") {
+
+            }
+
+          },
           "themes" : { "stripes" : true },
           'data' : this.testData
       }
-  });
+    }).on('loaded.jstree', () => {
+      $('#tree').jstree('open_all');
+    });
   }
 
   create() {
     let ref = $('#tree').jstree(true),
         sel = ref.get_selected();
     if (!sel.length) { return false; }
+    console.log(sel);
     sel = sel[0];
     sel = ref.create_node(sel, {"type":"file"});
     if (sel) {
